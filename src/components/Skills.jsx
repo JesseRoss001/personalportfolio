ef115d1a78d88bf8c63d36f450432c21e082136a
Jesse Lee Ross <79084912+JesseRoss001@users.noreply.github.com>
2024-01-04 20:35:58 +0000

    Style skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,10 +1,28 @@
+import React from 'react';
 import { useAppContext } from "../appContext";
 import { Element } from "react-scroll";
-// Data
 import { skillData, resume } from "../data";
-// Components
 import { Button, Col, Container, Row } from "react-bootstrap";
 import { Title } from "./globalStyledComponents";
+import styled from 'styled-components';
+
+const GlassEffect = styled.div`
+background: ${({ theme }) => theme === "light" ? "rgba(255, 255, 255, 0.85)" : "#333"}
+
+  backdrop-filter: blur(10px);
+  border-radius: 10px;
+  
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.4);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  height: 140px; // Adjust as needed
+  width: 100px; // Adjust as needed
+  margin: auto; // Center within the column
+  padding:0px;
+`;
 
 export default function Skills() {
   const { theme } = useAppContext();
@@ -20,11 +38,13 @@ export default function Skills() {
           <Row className="mt-3 align-items-center">
             {skillData.map((skills) => {
               return (
-                <Col lg={2} md={3} xs={4} key={skills.id} className="my-md-5">
+                <Col lg={2} md={3} xs={4} key={skills.id} className="my-md-5 ps-0 pe-0 pb-2">
+                  <GlassEffect>
                   <figure>
                     {skills.skill}
                     <figcaption>{skills.name}</figcaption>
                   </figure>
+                  </GlassEffect>
                 </Col>
               );
             })}
